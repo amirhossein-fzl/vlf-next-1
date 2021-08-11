be19159f83e76c8264935a96cd0956a9af465db7
Amirhossein Fazli <amirhossein95b@gmail.com>
2021-08-11 13:29:01 +0430

    Initial projects

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    outDir: 'dist',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    poweredByHeader: false,
+    images: {
+        loader: 'imgix',
+        path: '/',
+    },
+};
