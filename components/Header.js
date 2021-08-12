2267578e7870b63b4d26589348cfdf79fc83ff12
Amirhossein Fazli <amirhossein95b@gmail.com>
2021-08-12 15:41:18 +0430

    chnage primary and secondary palette colors

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Header(props) {
     return (
         <header>
+            {/* <div>
+                <Button variant="contained" color="primary" size="small" disableElevation>
+                    پرایمری
+                </Button>
+                <Button variant="outlined" color="primary" size="small" disableElevation>
+                    اوتلایند
+                </Button>
+                <Button color="primary" size="small" disableElevation>
+                    فلت
+                </Button>
+            </div>
+
+            <div>
+                <Button variant="contained" color="secondary" size="small" disableElevation>
+                    سکونداری
+                </Button>
+                <Button variant="outlined" color="secondary" size="small" disableElevation>
+                    اوتلایند
+                </Button>
+                <Button color="secondary" size="small" disableElevation>
+                    فلت
+                </Button>
+            </div> */}
+            
         </header>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import { createTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import '../styles/globals.scss';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#4361ee",
+            dark: "#4361ee",
+            light: "#4361ee",
+        },
+        secondary: {
+            main: "#bc00dd",
+            dark: "#bc00dd",
+            light: "#bc00dd",
+        },
+    },
+    typography: {
+        fontFamily: ['IRANSans'],
+    },
+});
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </MuiThemeProvider>
+    );
 }
 
 export default MyApp;
