fc5111e587a918f43481420c3193982d4d3e2914
Amirhossein Fazli <amirhossein95b@gmail.com>
2021-10-13 10:52:37 +0330

    reomve preload styles

--- pages/_document.js
+++ pages/_document.js
@@ -62,7 +62,6 @@ MyDocument.getInitialProps = async (ctx) => {
         <style
             data-emotion={`${style.key} ${style.ids.join(' ')}`}
             key={style.key}
-            rel="preload"
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: style.css }}
         />
